e6a2096c82b1fb272d96c8c8b352a4c3a364e99b
EazyM93 <centini.manuel.93@gmail.com>
2023-05-16 23:50:32 +0200

    up

---
+++ assets/js/homepage.js
@@ -0,0 +1,83 @@
+// array suggeriti
+const suggeriti = [
+    {
+        suggName: 'La tua estate da film',
+        suggImg: './assets/img/main/image-2.jpg',
+        description: 'La colonna sonora della tua estate'
+    },
+    {
+        suggName: 'Street Culto',
+        suggImg: './assets/img/main/image-19.jpg',
+        description: 'La migliore musica rap underground' 
+    },
+    {
+        suggName: 'Massima Concentrazione',
+        suggImg: './assets/img/main/image-3.jpg',
+        description: 'Musica di sottofondo' 
+    },
+    {
+        suggName: 'Peaceful Piano',
+        suggImg: './assets/img/main/image-4.jpg',
+        description: 'Musica classica per tutti i gusti' 
+    },
+    {
+        suggName: 'Estate 2022',
+        suggImg: './assets/img/main/image-18.jpg',
+        description: 'Classici intramontabili' 
+    },
+    {
+        suggName: 'Lavora da casa',
+        suggImg: './assets/img/main/image-16.jpg',
+        description: 'Lavora al meglio' 
+    },
+    {
+        suggName: 'Casa Dolce Casa',
+        suggImg: './assets/img/main/image-15.jpg',
+        description: 'Day-off & chill' 
+    },
+    {
+        suggName: 'lofi Beats',
+        suggImg: './assets/img/main/image-5.jpg',
+        description: 'Se cerchi ispirazione' 
+    },
+    {
+        suggName: 'New Music Friday',
+        suggImg: './assets/img/main/image-17.jpg',
+        description: 'Nuove uscite settimanali' 
+    },
+    {
+        suggName: 'Jazz Vibes',
+        suggImg: './assets/img/main/image-6.jpg',
+        description: 'Libera la mente' 
+    },
+    {
+        suggName: 'Caffè del Buongiorno',
+        suggImg: './assets/img/main/image-7.jpg',
+        description: 'Per una marcia in più' 
+    },
+    {
+        suggName: 'sanguegiovane',
+        suggImg: './assets/img/main/image-8.jpg',
+        description: 'Band emergenti' 
+    },
+    {
+        suggName: 'Canta Sotto la Doccia',
+        suggImg: './assets/img/main/image-9.jpg',
+        description: 'Libera il cantante che è in te' 
+    },
+    {
+        suggName: 'Operazione Buonumore',
+        suggImg: './assets/img/main/image-10.jpg',
+        description: 'Ma perchè i capelli in faccia?' 
+    },
+    {
+        suggName: 'Pulizie di casa',
+        suggImg: './assets/img/main/image-13.jpg',
+        description: 'Ti facciamo compagnia' 
+    },
+    {
+        suggName: 'Allenamento a Casa',
+        suggImg: './assets/img/main/image-14.jpg',
+        description: 'Allenati al massimo' 
+    },
+]
