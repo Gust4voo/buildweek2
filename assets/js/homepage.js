53793a842d58094f1546631724eec17e61d682f2
EazyM93 <centini.manuel.93@gmail.com>
2023-05-16 19:34:52 +0200

    Update homepage.js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -35,11 +35,11 @@ async function singleFriend(obj){
 
     row.classList.add('row', 'd-flex', 'mb-4', 'backgroundBlack');
 
-    row.innerHTML = `<div class="col-8 d-flex">
+    row.innerHTML = `<div class="col-8 d-flex text-truncate">
         <div class="me-2">
-            <img src="${obj.firendImg}" class="img-fluid rounded-circle" width="40px" alt="...">
+            <img src="${obj.firendImg}" class="rounded-circle" width="40px" alt="...">
         </div>
-        <div>
+        <div class="">
             <p style="font-size: 0.9em;" class="m-0">${obj.friendName}</p>
             <p style="font-size: 0.7em;" class="m-0">${currentSong.title}<i class="bi bi-dot"></i>${currentSong.artist.name}</p>
             <p style="font-size: 0.7em;" class="m-0"><i class="bi bi-disc"></i> ${currentSong.album.title}</p>
@@ -59,11 +59,47 @@ function loadFriends(){
 
 }  
 
-
+// ---------- LOAD NAVBAR ----------
 
 
 
 // ---------- FETCHES ----------
+
+// playlist array
+const playlistNames = [
+    "Be The Young Seasons 1-5",
+    "Be The Young Seasons 6-8",
+    "persona di m*rda (gen-feb 2023)",
+    "Musical 2022",
+    "pippo, pluto e paperino (nov-dec 2022)",
+    "early stage emily syndrome (sett-ott 2022)",
+    "Be the young",
+    "'...' cit. Kimiko (lug-ago 2022)",
+    "saggio vibes 💃🩰",
+    "main character energy (mag-giu 2022)",
+    "that fucking mood 🔪☠️",
+    "VEE, CARLOTTA E GIACOMO VANNO A BOSIO",
+    "An Emily Winchester kind of mood 🔪🖕",
+    "landing page (mar-apr 2022)",
+    "2021 lol",
+    "cosa cazzo vuol dire questa affermazione (gen-feb 2022)",
+    "honey and glass (nov-dic 2021)",
+    "(Revenge) Training Arc 🦍",
+    "Lidia 🤝 Emily",
+    "minecraft e nintendo switch (sep-oct 2021)",
+    "silvano d'orba? I hardly know her (lug - ago 2021)",
+    "Culo 2021",
+    "Frah Quintale Mix",
+    "Francesco Guccini Mix",
+    "Lo Stato Sociale Mix",
+    "chapter 4/? (mag-giu 2021)",
+    "Strive School <> The Hunt Motivation",
+    "mi stavo dimenticando (mar-apr 2021)",
+    "high school musical 1,2,3",
+    "quanto trash cazzo",
+    "The 2020 Playlist",
+    "ma(ncanza) che cazzo ne so io (gen-feb 2021)",
+];
 
 // search fetch
 const search = async (param) => {
